bbd5e87c6e256e53969949afedc39992a6d88567
Saiteja <saiteja@Saitejas-MacBook-Pro.local>
2021-02-01 08:14:14 -0800

    Update  Order Service with offer tests

--- src/test/kotlin/order/app/OrderServiceAppWithOfferTest.kt
+++ src/test/kotlin/order/app/OrderServiceAppWithOfferTest.kt
@@ -11,10 +11,9 @@ import org.junit.Assert
 import org.junit.Test
 import java.math.BigDecimal
 import java.text.NumberFormat
-import java.util.*
+import java.util.HashMap
 
-
-class OrderServiceAppTest {
+class OrderServiceAppWithOfferTest {
     private var basket = Basket()
 
     val price_5: Price = Price(BigDecimal(0.60))
@@ -42,64 +41,45 @@ class OrderServiceAppTest {
         offerMap.put(Constants.ORANGE, orangeOffer)
     }
 
-    @Test
-    fun testAddMultipleQuantityofProductWithRemovingOneUnit() {
-        setup()
-
-        basket.addCartItem(Item_1)
-        basket.addCartItem(Item_2)
-        basket.addCartItem(Item_3)
-        basket.addCartItem(Item_4)
-
-        Assert.assertTrue(2 == basket.totalLineItem)
-        Assert.assertTrue(3 == basket.cartItems.get(0).getQuantity())
-
-        basket.decreaseByOne(Item_4)
-        Assert.assertTrue(1 == basket.totalLineItem)
-        Assert.assertTrue(3 == basket.cartItems.get(0).getQuantity())
-    }
 
     @Test
-    fun testAddApplesVerifyTotalWithoutOffer() {
+    fun testAddAppleProductsWithBuyOneGetOne() {
         setup()
-        //3 apples
         basket.addCartItem(Item_1)
         basket.addCartItem(Item_2)
-        basket.addCartItem(Item_3)
-
-        //remove offer
-        basket.offerMap = Collections.emptyMap()
 
         Assert.assertTrue(1 == basket.totalLineItem)
         basket.refreshTotalDiscount()
         val totalPrice = NumberFormat.getCurrencyInstance().format(basket.totalPriceBeforeDiscount)
 
-        val totalPriceAfterDiscount = NumberFormat.getCurrencyInstance().format(basket.totalPriceAfterDiscount)
+        val totalDiscount = NumberFormat.getCurrencyInstance().format(basket.totalPriceAfterDiscount)
+        val totalPriceAfterDiscount = NumberFormat.getCurrencyInstance().format(basket.totalDiscount)
+        Assert.assertTrue("$1.20" == totalPrice)
+        Assert.assertTrue("$0.60" == totalDiscount)
+        Assert.assertTrue("$0.60" == totalPriceAfterDiscount)
 
-        Assert.assertTrue("$1.80" == totalPrice)
-        Assert.assertTrue("$1.80" == totalPriceAfterDiscount)
     }
 
     @Test
-    fun testAddOrangesVerifyTotalWithoutOffer() {
+    fun testAddOrangeProductsWithBuy3InGet2Price() {
         setup()
-        //3 oranges
         basket.addCartItem(Item_4)
         basket.addCartItem(Item_4)
         basket.addCartItem(Item_4)
-
-        //remove offer
-        basket.offerMap = Collections.emptyMap()
 
         Assert.assertTrue(1 == basket.totalLineItem)
         basket.refreshTotalDiscount()
         val totalPrice = NumberFormat.getCurrencyInstance().format(basket.totalPriceBeforeDiscount)
 
-        val totalPriceAfterDiscount = NumberFormat.getCurrencyInstance().format(basket.totalPriceAfterDiscount)
+        val totalDiscount = NumberFormat.getCurrencyInstance().format(basket.totalPriceAfterDiscount)
+        val totalPriceAfterDiscount = NumberFormat.getCurrencyInstance().format(basket.totalDiscount)
 
         Assert.assertTrue("$0.75" == totalPrice)
-        Assert.assertTrue("$0.75" == totalPriceAfterDiscount)
+        Assert.assertTrue("$0.25" == totalDiscount)
+        Assert.assertTrue("$0.50" == totalPriceAfterDiscount)
+
     }
+
 
 
 }
